08b6f691cf576613bfe26ad1ac376b620fefbf07
timo zuidinga <timodevemail@gmail.com>
2023-02-15 22:24:31 +0100

    addded login and inschijf

--- database.sql
+++ database.sql
@@ -1,14 +1,26 @@
-CREATE DATABASE `Deepdive` ; 
-USE `Deepdive` ;
+DROP DATABASE IF EXISTS `Deepdive`;
+
+CREATE DATABASE `Deepdive`;
+
+USE `Deepdive`;
+
 CREATE TABLE `users` (
     `userid` int(11) NOT NULL,
     `Name` varchar(45) DEFAULT NULL,
     `Wachtwoord` varchar(45) DEFAULT NULL,
-    `Rechten` varchar(45) DEFAULT NULL
-    PRIMARY KEY (`userid`)
+    `Rechten` varchar(45) DEFAULT NULL PRIMARY KEY (`userid`)
 );
 
 INSERT INTO
     `users` (`userid`, `Name`, `Wachtwoord`, `Rechten`)
 VALUES
     (1, 'Admin', 'Admin', 'Admin');
+
+CREATE TABLE lidmaatschap(
+    `userid` INT AUTO_INCREMENT PRIMARY KEY,
+    `voornaam` varchar(45) DEFAULT NULL,
+    `achternaam` varchar(45) DEFAULT NULL,
+    `email` varchar(50) NOT NULL,
+    `telefoon` int NOT NULL,
+    `bericht` text
+);
---
+++ public/php/inschijf_forolier.php
@@ -0,0 +1,47 @@
+<?php
+
+require("../../conn.php");
+if (isset($_POST["opslaan"])) {
+    $kaas = "INSERT INTO `lidmaatschap` (`voornaam`,`achternaam`,`email`,`telefoon`,`bericht`)
+    VALUES(       
+        '{$_POST['voornaam']}',
+        '{$_POST['achternaam']}',
+        '{$_POST['email']}',
+        '{$_POST['nummer']}',
+        '{$_POST['bericht']}')";
+    $add_update = $dbh->query($kaas);
+    // header("Location: index.php");
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="header">
+        <h1>voeg een film toe aan de website</h1>
+        <h4><a href="index.php">ga terug naar de hoofd pagina</a></h4>
+    </div>
+    <div id="contaner">
+        <form id="formbrr" method="POST">
+            voornaam<input type="text" name="voornaam">
+            achternaam<input type="text" name="achternaam">
+            email<input type="text" name="email">
+            telefoon nummer<input type="tel" name="nummer">
+            bericht<textarea cols="30" rows="10" name="bericht"></textarea>
+            <input type="submit" name="opslaan"></input>
+        </form>
+    </div>
+
+</body>
+
+</html>
